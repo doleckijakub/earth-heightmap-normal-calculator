77654119f2105338b2c602c2d7051cb2debccbbd
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-04-02 18:54:25 +0200

    Added .github
    Implemented writing file

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
+earth-heightmap-normal-calculator: main.c stb.o
+	cc -o $@ $^ -lm
+
 test: earth-heightmap-normal-calculator
-	./earth-heightmap-normal-calculator gebco_08_rev_elev_21600x10800.png output.png
+	./earth-heightmap-normal-calculator .github/gebco_08_rev_elev_540x270.png .github/output_540x270.png
 
 stb.o: stb.c
 	cc -c -o $@ $<
 
-earth-heightmap-normal-calculator: main.c stb.o
-	cc -o $@ $^ -lm
--- main.c
+++ main.c
@@ -11,21 +11,27 @@ int main(int argc, const char **argv) {
 	}
 
 	const char *input  = argv[1];
-	{
-		int w, h, n;
-		unsigned char *data = stbi_load(input, &w, &h, &n, 0);
-		assert(n == 1 && "Not grayscale");
-
-		for (int x = 0; x < w; x++) {
-			for (int y = 0; y < h; y++) {
-				
-			}
-		}
-
-		stbi_image_free(data);
-	}
-
 	const char *output = argv[2];
 
+	int w, h, n;
+	unsigned char *in = stbi_load(input, &w, &h, &n, 1);
+	unsigned char *out = calloc(w * h, 4);
+	assert(n == 1 && "Not grayscale");
+
+	int i = 0, j = 0;
+	for (int x = 0; x < w; x++) {
+		for (int y = 0; y < h; y++) {
+			out[i++] = 255;
+			out[i++] = 0;
+			out[i++] = 0;
+			out[i++] = in[j++];
+		}
+	}
+
+	stbi_image_free(in);
+
+	stbi_write_png(output, w, h, 4, out, 4 * w);
+
+	free(out);
 
 }
